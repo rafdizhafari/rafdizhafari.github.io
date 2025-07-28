3f07e572998c728303d792d18fd23c9b2c7b65f5
ererzet <reyhanzhafari@gmail.com>
2025-07-28 17:07:46 +0700

    move django

--- Comment/venv/commentBackend/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("submit/",views.submit_form,name='submit_form'),
-    path("show/",views.comment_show),
-    path("afterInput/",views.after_input)
-]
